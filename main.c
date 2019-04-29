42a251892fda53827545372a3cbaa045dffe6f1c
James Jordan <jameskjordan23@gmail.com>
2019-04-29 03:48:49 +0000

    start

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
-#include <stdio.h>
+/* This C program will Encrypt and decrypt words given to it using the rotation cipher and
+    substitution cipher methods. a series of functions will be used to achieve this. 
+    choosing between tasks will be done by entering in a intial variable.
+ */
 
+#include <stdio.h>
 int main()
 {
-   int a,b,c;
-   scanf("%d %d %d", &a, &b, &c);
-   printf("found int %d %d %d\n", a, b, c);
-    
+    int task;
+    printf("enter task number: \n");
+    scanf("%d", &task);
+    printf("task number is: %d\n", task);
+    switch(task){
+        case 1: printf("task 1"); 
+        
+        break;
+        case 2: printf("task 2");
+        break;
+    }
+   
+   
+   
+   
     
     return 0;
 }
